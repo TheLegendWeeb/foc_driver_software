a3fc19e9b68c06076dec9695bcd9453a0203f305
TheLegendWeeb <sebibucur2002@gmail.com>
2025-04-07 22:48:24 +0300

    made sin lookup table generation occur at startup instead of using premade lookup table from python script

--- built_sine_table.py
+++
@@ -1,8 +0,0 @@
-import math
-
-resolution=1024
-lookup_table=[]
-for i in range(0,resolution):
-    lookup_table.append(round(math.sin(i*(math.pi/2)/resolution),10))
-with open("lookup_table.txt","w+") as f:
-    f.write("".join(str(lookup_table)))
